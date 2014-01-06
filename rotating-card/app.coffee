2484591480055d414e174b159cbc0ad851406450
benny-z <bennyn@wearezeta.com>
2014-01-06 17:13:28 +0100

    Added rotating-card demo which works in FF 26, Chrome 31 and partly in IE 10.

---
+++ rotating-card/app.coffee
@@ -0,0 +1,4 @@
+$(document).ready ->
+  $('.card').on('click', ->    
+    $(@).addClass 'flip'
+  );
---
+++ rotating-card/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  return $('.card').on('click', function() {
+    return $(this).addClass('flip');
+  });
+});
